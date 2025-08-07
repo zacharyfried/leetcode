31503b5c707b236446b37cf7a762e3152b4a42ba
Zachary Fried <zatfried@gmail.com>
2025-08-06 21:28:49 -0400

    Solved 242. Valid Anagram – O(n) time using fixed-size int[26] array

---
+++ LC242_Valid_Anagram.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class LC242_Valid_Anagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.isEmpty() || t.isEmpty() || s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int num: count){
+            if (num != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
