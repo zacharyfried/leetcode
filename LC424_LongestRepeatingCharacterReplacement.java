9cbe6a5f3e0faca0b2e4ac0e94054d38c9eda6cd
Zachary Fried <zatfried@gmail.com>
2025-08-20 23:33:39 -0400

    Check solved 424. Longest Repeating Character Replacement - O(n) solution with freq int array

--- LC424_LongestRepeatingCharacterReplacement.java
+++ LC424_LongestRepeatingCharacterReplacement.java
@@ -2,36 +2,24 @@ import java.util.HashSet;
 
 public class LC424_LongestRepeatingCharacterReplacement {
     public int characterReplacement(String s, int k) {
+        int maxCount = 0;
         int res = 0;
+        int l = 0;
+        int[] freq = new int[26];
 
-        HashSet<Character> chars = new HashSet<>();
+        for (int r = 0; r < s.length(); r++){
+            int index = s.charAt(r) - 'A';
+            freq[index]++;
+            maxCount = Math.max(maxCount, freq[index]);
 
-        for (char c : s.toCharArray()){
-            chars.add(c);
-        }
-
-        for (char c : chars){
-            int count = 0;
-            int l = 0;
-
-            for (int r = 0; r < s.length(); r++){
-                if (s.charAt(r) == c){
-                    count++;
-                }
-
-                // Window size minus count (of non-changed chars)
-                // is essentially the number of changes chars
-                // So like while we have to many changes chars, we move l
-                while ((r - l + 1) - count > k){
-                    if (s.charAt(l) == c){
-                        count--; // we have one less unchanged char in window
-                    }
-                    l++;
-                }
-                res = Math.max(res, (r - l + 1));
+            while ((r - l + 1) - maxCount > k){
+                freq[s.charAt(l) - 'A']--;
+                l++;
             }
 
+            res = Math.max(res, (r - l + 1));
         }
+
         return res;
     }
 }
