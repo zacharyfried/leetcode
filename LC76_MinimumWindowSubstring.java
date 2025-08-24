264ad965007711b7952d95543118362ba4851658
Zachary Fried <zatfried@gmail.com>
2025-08-24 19:05:01 -0400

    Check Solved 76. Minimum Window Substring - O(n) solution

--- LC76_MinimumWindowSubstring.java
+++ LC76_MinimumWindowSubstring.java
@@ -1,50 +1,54 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class LC76_MinimumWindowSubstring {
     public String minWindow(String s, String t) {
-        String res = "";
+        if (t.length() == 0 || s.length() < t.length()) return "";
 
-        HashMap<Character, Integer> sMap = new HashMap<>();
-        HashMap<Character, Integer> tMap = new HashMap<>();
-
+        // Frequency of chars in t
+        int[] need = new int[128];
+        int required = 0; // number of unique chars in t
         for (char c : t.toCharArray()){
-            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+            if (need[c] == 0){
+                required++; // indicate a new distinct char
+            }
+            need[c]++; // increase frequency of this char
         }
 
+        int[] have = new int[128]; // freq array for window
+        int formed = 0; // tracks all chars where we have all needed
+
         int l = 0;
+        int bestL = 0; // l pointer for best window
+        int bestLen = Integer.MAX_VALUE;
 
         for (int r = 0; r < s.length(); r++){
-            sMap.put(s.charAt(r), sMap.getOrDefault(s.charAt(r), 0) + 1);
-            while (containsT(sMap, tMap)){
-                if (res.length() == 0){
-                    res = s.substring(l, r + 1);
-                } else if (res.length() > (r - l + 1)) {
-                    res = s.substring(l, r + 1);
+            char cr = s.charAt(r);
+            have[cr]++;
+
+            // Update formed if we have all of this char
+            if (need[cr] > 0 && have[cr] == need[cr]){
+                formed++;
+            }
+
+            // Shrink while valid (if valid at all)
+            while (formed == required){
+                int winLen = r - l + 1;
+                if (winLen < bestLen){
+                    bestLen = winLen;
+                    bestL = l;
                 }
-                sMap.put(s.charAt(l), sMap.get(s.charAt(l)) - 1);
+
+                char cl = s.charAt(l);
+                have[cl]--;
+                // If removal of this char messes up validity formed = req
+                if (need[cl] > 0 && have[cl] < need[cl]){
+                    formed--;
+                }
                 l++;
             }
         }
-
-
-        return res;
+        return bestLen == Integer.MAX_VALUE ? "" : s.substring(bestL, bestL + bestLen);
     }
 
-    private static boolean containsT(HashMap<Character, Integer> sMap, HashMap<Character, Integer> tMap){
-        for (Map.Entry<Character, Integer> entry : tMap.entrySet()){
-            if (sMap.getOrDefault(entry.getKey(), 0) < entry.getValue()){
-                return false;
-            }
-        }
-        return true;
-    }
 
-    public static void main(String[] args){
-        String s = "ADOBECODEBANC";
-        String t = "ABC";
-
-        LC76_MinimumWindowSubstring obj = new LC76_MinimumWindowSubstring();
-        System.out.println(obj.minWindow(s, t));
-    }
 }
