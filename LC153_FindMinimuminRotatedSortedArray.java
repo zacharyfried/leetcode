08129cd646fe4e320080178070b95c449637cd9c
Zachary Fried <zatfried@gmail.com>
2025-09-05 17:25:28 -0400

    Solved 153. Find Minimum Rotated Sorted Array - cleaner solutation than original with single if else deciding change in l and r pointers

--- LC153_FindMinimuminRotatedSortedArray.java
+++ LC153_FindMinimuminRotatedSortedArray.java
@@ -2,22 +2,25 @@ import java.util.*;
 
 public class LC153_FindMinimuminRotatedSortedArray {
     public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
 
-        int a = 0;
-        int b = nums.length - 1;
-        int c = 0;
-
-        while(Math.abs(a-b) > 1){
-            c = (a + b) / 2;
-            if (nums[a] < nums[c] && nums[c] < nums[b]){
-                b = c;
-            } else if (nums[a] > nums[c] && nums[c] < nums[b]){
-                b = c;
+        while (l < r){
+            int m = l + (r - l) / 2;
+            if (nums[m] > nums[r]){
+                l = m + 1;
             } else {
-                a = c;
+                r = m;
             }
         }
 
-        return Math.min(nums[a], nums[b]);
+        return nums[l];
+
+    }
+
+    public static void main(String[] args){
+        LC153_FindMinimuminRotatedSortedArray obj = new LC153_FindMinimuminRotatedSortedArray();
+
+        System.out.println(obj.findMin(new int[]{3,4,5,1,2}));
     }
 }
